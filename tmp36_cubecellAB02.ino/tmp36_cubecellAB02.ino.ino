9f66d5bb75c300897b297f82b9604c97dcfa46c2
Simon Moinard <simmoinard@gmail.com>
2022-07-06 16:49:25 +0200

    Create tmp36_cubecellAB02.ino.ino
    ajout d'un code pour envoyer les données de 2 capteurs de température

---
+++ tmp36_cubecellAB02.ino/tmp36_cubecellAB02.ino.ino
@@ -0,0 +1,105 @@
+#include "LoRaWanMinimal_APP.h"
+#include "Arduino.h"
+
+//Set these OTAA parameters to match your app/node in TTN
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x05, 0x2E, 0x57 };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = { 0x18, 0xCC, 0x65, 0x2D, 0x44, 0xBD, 0xB0, 0xDC, 0x5D, 0xFB, 0xEC, 0xEA, 0x36, 0xCE, 0x11, 0x41 };
+int temps = 120; // Indiquez dans cette ligne la fréquence d'envoi de données, en secondes. (Ne pas aller plus bas que 3minutes, soit 180sec)
+
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+static uint8_t counter=0;
+uint8_t lora_data[5];
+uint8_t downlink ;
+
+///////////////////////////////////////////////////
+//Some utilities for going into low power mode
+TimerEvent_t sleepTimer;
+//Records whether our sleep/low power timer expired
+bool sleepTimerExpired;
+
+static void wakeUp()
+{
+  sleepTimerExpired=true;
+}
+
+static void lowPowerSleep(uint32_t sleeptime)
+{
+  sleepTimerExpired=false;
+  TimerInit( &sleepTimer, &wakeUp );
+  TimerSetValue( &sleepTimer, sleeptime );
+  TimerStart( &sleepTimer );
+  //Low power handler also gets interrupted by other timers
+  //So wait until our timer had expired
+  while (!sleepTimerExpired) lowPowerHandler();
+  TimerStop( &sleepTimer );
+}
+
+///////////////////////////////////////////////////
+void setup() {
+	Serial.begin(115200);
+  
+  LoRaWAN.begin(LORAWAN_CLASS, ACTIVE_REGION);
+  
+  //Enable ADR
+  LoRaWAN.setAdaptiveDR(true);
+
+  while (1) {
+    Serial.print("Joining... ");
+    LoRaWAN.joinOTAA(appEui, appKey, devEui);
+    if (!LoRaWAN.isJoined()) {
+      //In this example we just loop until we're joined, but you could
+      //also go and start doing other things and try again later
+      Serial.println("JOIN FAILED! Sleeping for 30 seconds");
+      lowPowerSleep(30000);
+    } else {
+      Serial.println("JOINED");
+      break;
+    }
+  }
+}
+
+
+///////////////////////////////////////////////////
+void loop()
+{
+  counter++; 
+  delay(10);
+  uint8_t voltage = getBatteryVoltage()/50; //Tension en %
+
+  Serial.printf("\nVoltage : %d\n", voltage);
+
+  int sensorValue2 = analogRead(ADC2);
+  int sensorValue3 = analogRead(ADC3);
+  
+  float tension2 = (sensorValue2)/4094.00*2.40;
+  float tempfloat2 = (tension2 - 0.50) * 100 ;
+  int temp2 = tempfloat2 *100 + 5000;
+  float tension3 = (sensorValue3)/4094.00*2.40;
+  float tempfloat3 = (tension3 - 0.50) * 100;
+  int temp3 = tempfloat3 *100 + 5000;
+
+  Serial.printf("\nTemp2 = %d\n", tempfloat2);
+  Serial.printf("\nTemp3 = %d\n", tempfloat3);
+
+  lora_data[0] = voltage;
+  lora_data[1] = highByte(temp2);
+  lora_data[2] = lowByte(temp2);
+  lora_data[3] = highByte(temp3);
+  lora_data[4] = lowByte(temp3);
+  
+  //Now send the data. The parameters are "data size, data pointer, port, request ack"
+  Serial.printf("\nSending packet with counter=%d\n", counter);
+  Serial.printf("\nValue to send : %d\n", lora_data);
+
+  //Here we send confirmed packed (ACK requested) only for the first two (remember there is a fair use policy)
+  bool requestack=counter<2?true:false;
+  if (LoRaWAN.send(sizeof(lora_data), lora_data, 1, requestack)) {
+    Serial.println("Send OK");
+  } else {
+    Serial.println("Send FAILED");
+  }
+
+  lowPowerSleep(temps*1000);  
+  delay(10);
+}
