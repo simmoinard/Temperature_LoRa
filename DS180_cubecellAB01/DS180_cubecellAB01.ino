5f4933dab4bdce51895d1a53b27d9c2b370074b4
Simon Moinard <simmoinard@gmail.com>
2022-12-13 10:33:35 +0100

    ds18B20 with AB01

---
+++ DS180_cubecellAB01/DS180_cubecellAB01.ino
@@ -0,0 +1,188 @@
+#include <OneWire.h>
+#include "LoRaWanMinimal_APP.h"
+#include "Arduino.h"
+
+//Set these OTAA parameters to match your app/node in TTN
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x05, 0x87, 0x18 };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = { 0x81, 0x51, 0xAD, 0xF8, 0x27, 0x52, 0x06, 0xB1, 0x23, 0x28, 0x79, 0x0B, 0x7E, 0x3F, 0x4D, 0x29 };
+int temps = 120; // Indiquez dans cette ligne la fréquence d'envoi de données, en secondes. (Ne pas aller plus bas que 3minutes, soit 180sec)
+
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+static uint8_t counter=0;
+uint8_t lora_data[3];
+uint8_t downlink ;
+
+///////////////////////////////////////////////////
+//Some utilities for going into low power mode
+TimerEvent_t sleepTimer;
+//Records whether our sleep/low power timer expired
+bool sleepTimerExpired;
+
+static void wakeUp()
+{
+  sleepTimerExpired=true;
+}
+
+static void lowPowerSleep(uint32_t sleeptime)
+{
+  sleepTimerExpired=false;
+  TimerInit( &sleepTimer, &wakeUp );
+  TimerSetValue( &sleepTimer, sleeptime );
+  TimerStart( &sleepTimer );
+  //Low power handler also gets interrupted by other timers
+  //So wait until our timer had expired
+  while (!sleepTimerExpired) lowPowerHandler();
+  TimerStop( &sleepTimer );
+}
+
+OneWire  ds(GPIO1);  // on pin GPIO1 PIN 6 (a 4.7K resistor is necessary)
+
+void setup(void) {
+  //Vext ON
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext,LOW);
+  delay(500);
+
+  Serial.begin(115200);
+
+  LoRaWAN.begin(LORAWAN_CLASS, ACTIVE_REGION);
+  
+  //Enable ADR
+  LoRaWAN.setAdaptiveDR(true);
+
+  while (1) {
+    Serial.print("Joining... ");
+    LoRaWAN.joinOTAA(appEui, appKey, devEui);
+    if (!LoRaWAN.isJoined()) {
+      //In this example we just loop until we're joined, but you could
+      //also go and start doing other things and try again later
+      Serial.println("JOIN FAILED! Sleeping for 30 seconds");
+      lowPowerSleep(30000);
+    } else {
+      Serial.println("JOINED");
+      break;
+    }
+  }
+}
+
+void loop(void) {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  ds.search(addr);
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  
+  counter++; 
+  delay(10);
+  uint8_t voltage = getBatteryVoltage()/50; //Tension en %
+
+  Serial.printf("\nVoltage : %d\n", voltage);
+
+  int temp = celsius *100 + 5000;
+
+
+  Serial.printf("\nTemp = %d\n", temp);
+
+
+  lora_data[0] = voltage;
+  lora_data[1] = highByte(temp);
+  lora_data[2] = lowByte(temp);
+
+  
+  //Now send the data. The parameters are "data size, data pointer, port, request ack"
+  Serial.printf("\nSending packet with counter=%d\n", counter);
+  Serial.printf("\nValue to send : %d\n", lora_data);
+
+  //Here we send confirmed packed (ACK requested) only for the first two (remember there is a fair use policy)
+  bool requestack=counter<2?true:false;
+  if (LoRaWAN.send(sizeof(lora_data), lora_data, 1, requestack)) {
+    Serial.println("Send OK");
+  } else {
+    Serial.println("Send FAILED");
+  }
+
+  lowPowerSleep(temps*1000);  
+  delay(10);
+}
